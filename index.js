e97f6a7514ca828991b959457a4b70f3f4e48f93
derek chen <derek.g.chen@gmail.com>
2018-02-01 20:13:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function shout(string){
   return string.toUpperCase();
 }
+
+function whisper(string){
+  return string.toUpperCase();
+}
+
+function logShout(string){
+  return string.toUpperCase();
+}
+
+function logWhisper(string){
+  return string.toUpperCase();
+}
+
+function sayHiToGrandma(string){
+  return string.toUpperCase();
+}
